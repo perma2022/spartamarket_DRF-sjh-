177e988675ab2bdfaf189db71f1dcc9e21807302
sjh <per_ma@naver.com>
2024-05-02 15:01:13 +0900

    profile 업데이트

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,16 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.urls import path
-from accounts import views
+from . import views
+
+
+app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.AccountSignupAPIView.as_view()),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # path("<str:username>/", views.ProfileAPIView.as_view(), )
+    path("<str:username>/", views.AccountProfileAPIView.as_view(), name="profile"),
 ]
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.forms import UserChangeForm
+from .models import User
+
 
 
 class AccountSignupAPIView(APIView):
@@ -11,3 +15,21 @@ class AccountSignupAPIView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+
+
+class AccountProfileAPIView(APIView):
+    def post(self, request, username):
+        user = get_object_or_404(User, username=username, is_active=True)
+        serializer = UserSerializer(data=request.data, instance=user, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        
+# def profile(request):
+#     if request.method == "POST":
+#         pass
+#     else:
+#         form = UserChangeForm(instance=request.user)
+#     context = {"form": form}
+#     return render(request, "accounts/profile.html", context)
